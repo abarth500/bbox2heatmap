a2cf4e9c465c489e7a880a0a540f567f40dc71f9
Shohei Yokoyama <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2015-10-19 10:09:14 +0900

    init

---
+++ execute.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Shohei Yokoyama on 2015/10/15.
+ */
+
+var BBOX2Heatmap = require("index.js");
+BBOX2Heatmap.bbox("11.543283,48.12657,11.554785,48.137053");
+
--- index.js
+++ index.js
@@ -1,3 +1,104 @@
 /**
  * Created by Shohei Yokoyama on 2015/10/15.
  */
+
+function BBOX2Heatmap(bbox){
+    var maxUploadDate = Math.floor(Date.now() / 1000);
+    /*
+    var north = Math.max(bbox[0],bbox[2]);
+    var south = Math.min(bbox[0],bbox[2]);
+    var east  = Math.max(bbox[1],bbox[3]);
+    var west  = Math.min(bbox[1],bbox[3]);
+    */
+    try{
+        var opt = require(__dirname + "/value.json");
+    } catch(err){
+        if(err.code === 'MODULE_NOT_FOUND'){
+            console.log("Set your Flickr API key to ./values.json (See also value-original.json)");
+            process.exit();
+        }
+    }
+    var flickr_options  = opt['flickr_options'];
+    var async = require('async');
+    var shortid = require('shortid');
+    var output = __dirname + "/output/" + shortid.generate() + ".json";
+
+    async.waterfall([
+        openOutputFile,
+        openFlickrConnection,
+        doCrawl
+    ],errorHandler);
+
+    function openOutputFile(next){
+        var fs = require('fs');
+        fs.open(output, "w",'0666',function(err,fd){
+            fs.writeSync(fd,'{"bbox":['+bbox.join(',')+'],"data":['+"\n");
+            next(err,fd);
+        });
+    }
+
+    function openFlickrConnection(fd,next){
+        var Flickr = require("flickrapi");
+        Flickr.tokenOnly(flickr_options, function (errFlickr, flickr) {
+            if (errFlickr) {
+                next(errFlickr);
+            }else{
+                next(null,fd,flickr);
+            }
+        });
+    }
+    function doCrawl(fd,flickr,next){
+        console.log("Ready!");
+        var ids = {};
+        var nop = 0;
+        process.on('SIGINT', function() {
+            next(new Error('SIGINT'),fd,flickr);
+        });
+        async.forever(function(next){
+            console.log("["+maxUploadDate+"] "+ nop +" photographs");
+            flickr.photos.search(
+                {
+                    bbox:bbox.join(","),
+                    sort:"date-posted-desc",
+                    max_upload_date :maxUploadDate,
+                    extras:"date_upload,date_taken,url_sq,url_z,geo"
+                },
+                function (err, result) {
+                    result.photos.photo.forEach(function(photo) {
+                        if(typeof ids[photo.id] != "undefined"){
+                            return true;
+                        }
+                        ids[photo.id] = true;
+                        nop++;
+                        //console.log(photo);
+
+                        var line = {
+                            "id":photo.id,
+                            "owner":photo.owner,
+                            "dateupload":0 + photo.dateupload,
+                            "datetaken":photo.datetaken,
+                            "latitude":photo.latitude,
+                            "longitude":photo.longitude,
+                            "url_sq":photo.url_sq,
+                            "url_z":photo.url_z
+                        };
+                        require('fs').write(fd,JSON.stringify(line)+",\n");
+                        maxUploadDate = 0 + photo.dateupload;
+                    });
+                    //maxUploadDate--;
+                    next(null,fd,flickr);
+                }
+            );
+        });
+    }
+    function errorHandler(err,fd){
+        console.log('[Stop by user '+err+']\n\tResult is stored into '+output);
+        var fs = require('fs');
+        fs.write(fd,"{}]}",function(){
+            fs.close(fd);
+            console.error(err);
+            process.exit();
+        });
+    }
+}
+module.exports = BBOX2Heatmap;

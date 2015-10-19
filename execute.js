1fc4919364b454124a57177e4eae355bbf321445
Shohei Yokoyama <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2015-10-19 11:37:59 +0900

    add execute.js

--- execute.js
+++ execute.js
@@ -1,7 +1,30 @@
 /**
  * Created by Shohei Yokoyama on 2015/10/15.
  */
-
-var BBOX2Heatmap = require("index.js");
-BBOX2Heatmap.bbox("11.543283,48.12657,11.554785,48.137053");
-
+var argv = require('argv');
+var BBOX2Heatmap = require(__dirname + "/index.js");
+argv.option([
+    {
+        name: 'bbox',
+        short: 'B',
+        type: 'csv,float',
+        description: 'set Bounding BOX e.g.',
+        example: "'node bbox2heatmap/execute.js -B 11.543283,48.12657,11.554785,48.137053'"
+    }
+]);
+var args = argv.run();
+if (!args.options.bbox) {
+    console.error("Set BBOX. (see --help)");
+    process.exit();
+} else {
+    if (args.options.bbox.length == 4
+        && !isNaN(args.options.bbox[0])
+        && !isNaN(args.options.bbox[1])
+        && !isNaN(args.options.bbox[2])
+        && !isNaN(args.options.bbox[3])) {
+        BBOX2Heatmap(args.options.bbox.slice(0, 4));
+    } else {
+        console.error("Invalid BBOX. (see --help)");
+        process.exit();
+    }
+}
